8c19f5c41cb7c044319ee8e786f72e1fda93f481
zh5054 <844092563@qq.com>
2017-03-20 17:15:37 +0800

    本地库干活

---
+++ WWWWW/WWWWW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWWWW
+//
+//  Created by alixiu02 on 17/3/20.
+//  Copyright © 2017年 alixiu02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WWWWW/WWWWW/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WWWWW
+//
+//  Created by alixiu02 on 17/3/20.
+//  Copyright © 2017年 alixiu02. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog( @"我爱你是我独家的记忆");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
